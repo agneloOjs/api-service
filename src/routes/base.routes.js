9d302c86eab917f289381b248bf72a457787f503
AgneloFH <tech.agnelo@gmail.com>
2024-10-10 18:37:04 -0300

    added privateRoutes

--- src/routes/base.routes.js
+++ src/routes/base.routes.js
@@ -5,9 +5,12 @@
 
 import { Router } from 'express';
 import publicRoutes from './public.routes.js';
+import privateRoutes from './private.routes.js';
 
 const routes = Router();
 
 routes.use('/', publicRoutes);
+
+routes.use('/', privateRoutes);
 
 export default routes;
