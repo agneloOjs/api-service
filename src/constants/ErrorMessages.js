08ab2e690773248c002a35ef800de96ea7d8be12
AgneloFH <tech.agnelo@gmail.com>
2024-10-10 18:12:51 -0300

    added server.js

--- src/constants/ErrorMessages.js
+++ src/constants/ErrorMessages.js
@@ -8,5 +8,6 @@ export const ERROR_MESSAGES = {
   INVALID_TOKEN: 'Token inválido.',
   UNAUTHORIZED: 'Acesso não autorizado.',
   USER_NOT_FOUND: 'Usuário não encontrado.',
-  INTERNAL_SERVER_ERROR: 'Erro interno do servidor.'
+  INTERNAL_SERVER_ERROR: 'Erro interno do servidor.',
+  ERROR_WHEN_STARTING_SERVER: 'Erro ao iniciar o servidor'
 };
---
+++ src/server.js
@@ -0,0 +1,39 @@
+/**
+ * Inicialização do servidor.
+ * @module server
+ */
+
+import 'dotenv/config';
+import app from './app.js';
+import { connectDb } from './config/dbService.js';
+import { ERROR_MESSAGES } from './constants/ErrorMessages.js';
+
+class Server {
+  constructor() {
+    this.port = process.env.PORT || 8000;
+    this.process.env.CORS_ORIGIN;
+  }
+
+  /**
+   * Inicia o servidor
+   */
+  async start() {
+    try {
+      // Conecta ao banco de dados antes de iniciar o servidor.
+      await connectDb();
+
+      // Inicia o servidor após a conexão com o banco de dados.
+      app.listen(this.port, () => {
+        console.log(`Servidor rodando na porta ${this.url}:${this.port}`);
+        console.log(`Ambiente: ${process.env.NODE_ENV || 'development'}`);
+        console.log(`Data/Hora: ${new Date().toDateString()}`);
+      });
+    } catch (error) {
+      console.error(`${ERROR_MESSAGES.ERROR_WHEN_STARTING_SERVER}`);
+      process.exit(1);
+    }
+  }
+}
+
+const server = new Server();
+server.start();
