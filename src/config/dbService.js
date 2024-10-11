66e09cadd281f7451f36f965068d5b7826929286
AgneloFH <tech.agnelo@gmail.com>
2024-10-11 17:17:14 -0300

    refactoring dbService

--- src/config/dbService.js
+++ src/config/dbService.js
@@ -1,10 +1,10 @@
 import { PrismaClient } from '@prisma/client';
 
 const dbService = new PrismaClient();
+
 /**
  * Função para conectar ao banco de dados
  */
-
 export const connectDb = async () => {
   try {
     await dbService.$connect();
@@ -14,4 +14,18 @@ export const connectDb = async () => {
     throw error;
   }
 };
+
+/**
+ * Função para desconectar do banco de dados
+ */
+export const disconnectDb = async () => {
+  try {
+    await dbService.$disconnect();
+    console.log('Desconectado do banco de dados com sucesso.');
+  } catch (error) {
+    console.error('Erro ao desconectar do banco de dados:', error);
+    throw error;
+  }
+};
+
 export default dbService;
