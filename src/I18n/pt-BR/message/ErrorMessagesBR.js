c235217082b0c9bd5e4042b4823b761bf808ad3f
AgneloFH <tech.agnelo@gmail.com>
2024-10-11 15:45:30 -0300

    added 18n/pt-BR/message/ErrorMessagesBR

---
+++ src/I18n/pt-BR/message/ErrorMessagesBR.js
@@ -0,0 +1,11 @@
+/**
+ * @module 18n/pt-BR/message/ErrorMessagesBR
+ */
+
+export const ERROR_MESSAGES_BR = {
+  INVALID_TOKEN: 'Token inválido.',
+  UNAUTHORIZED: 'Acesso não autorizado.',
+  INTERNAL_SERVER_ERROR: 'Erro interno do servidor.',
+  ERROR_CREATING_COMPANY: 'Erro ao cadastrar empresa.',
+  ERROR_WHEN_STARTING_SERVER: 'Erro ao iniciar o servidor'
+};
