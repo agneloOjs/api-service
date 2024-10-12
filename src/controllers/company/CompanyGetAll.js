347070d0f8b93919799a6fa0794098b5795872ba
AgneloFH <tech.agnelo@gmail.com>
2024-10-12 00:07:03 -0300

    added class CompanyGetAllController

---
+++ src/controllers/company/CompanyGetAll.js
@@ -0,0 +1,22 @@
+import CompanyGetAllService from '../../services/company/CompanyGetAll.js';
+import { ERROR_MESSAGES_BR } from '../../I18n/pt-BR/message/ErrorMessagesBR.js';
+import Logger from '../../constants/Logger.js';
+
+export default class CompanyGetAllController {
+  constructor() {
+    this.companyGetAllService = new CompanyGetAllService();
+    this.getAll = this.getAll.bind(this);
+  }
+
+  async getAll(req, res) {
+    try {
+      const companies = await this.companyGetAllService.getAllCompanies();
+      return res.status(200).json(companies);
+    } catch (error) {
+      Logger.error(error);
+      return res
+        .status(500)
+        .json({ message: `${ERROR_MESSAGES_BR.INTERNAL_SERVER_ERROR}` });
+    }
+  }
+}
