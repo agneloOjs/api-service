38669528a38a05cf40ba74e89b507e5b10ec7c00
agnelofh <tech.agnelo@gmail.com>
2024-10-17 20:30:31 -0300

    added encryptPassword

---
+++ src/auth-service/utils/encryptPassword.js
@@ -0,0 +1,13 @@
+// auth.js
+import bcrypt from 'bcrypt';
+
+// Função para criptografar a senha
+export const encryptPassword = async (password) => {
+    try {
+        const saltRounds = 10; // Número de salt rounds
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        return hashedPassword; // Retorna a senha criptografada
+    } catch (error) {
+        throw new Error('Erro ao criptografar a senha: ' + error.message);
+    }
+};
