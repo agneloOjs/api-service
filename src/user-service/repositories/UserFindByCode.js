439e973939cc3a15d9f8040c2f6056660270547b
agnelofh <tech.agnelo@gmail.com>
2024-10-17 20:21:32 -0300

    added class UserFindByCode

---
+++ src/user-service/repositories/UserFindByCode.js
@@ -0,0 +1,8 @@
+import dbService from "../../config/dbService.js";
+
+export class UserFindByCode {
+  // Método da classe que busca o usuário pelo código
+  async findByCode(code) {
+    return await dbService.user.findUnique({ where: { code } });
+  }
+}
