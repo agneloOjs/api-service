e0ffa2ad45da40dbe284f206061c86b9678393d6
agnelofh <tech.agnelo@gmail.com>
2024-10-18 18:08:54 -0300

    refactoring generateTokenAccess

--- src/auth-service/utils/generateTokenAccess.js
+++ src/auth-service/utils/generateTokenAccess.js
@@ -6,14 +6,29 @@ const ACCESS_TOKEN_EXPIRATION = process.env.ACCESS_TOKEN_SECRET_EXPIRES_IN;
 /**
  * Gera um token JWT para o usuário.
  * @param {string} userId
- * @param {string} ipAddress
  * @param {string} deviceId
  * @param {string} deviceType
  * @returns {Promise<string>}
  */
-export const generateToken = (userId, ipAddress, deviceId, deviceType) => {
-  const token = jwt.sign({ userId }, ACCESS_TOKEN_SECRET, {
-    expiresIn: ACCESS_TOKEN_EXPIRATION
-  });
-  return token;
+export const generateTokenAccess = (userId, deviceId, deviceType) => {
+  const expiresAt = new Date(Date.now() + parseInt(ACCESS_TOKEN_EXPIRATION));
+
+  const payload = {
+    userId,
+    expiresAt: expiresAt.toISOString(),
+    ipAddress: '',
+    deviceId,
+    deviceType
+  };
+
+  try {
+    const token = jwt.sign(payload, ACCESS_TOKEN_SECRET, {
+      expiresIn: ACCESS_TOKEN_EXPIRATION
+    });
+
+    return token;
+  } catch (error) {
+    console.error('Erro ao gerar o token:', error);
+    throw new Error('Erro ao gerar o token');
+  }
 };
