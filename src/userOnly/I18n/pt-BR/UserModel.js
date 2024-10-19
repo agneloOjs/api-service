152efd08a92141132e02f979ff3cc28c1761a837
agnelofh <tech.agnelo@gmail.com>
2024-10-19 18:37:09 -0300

    added I18n/pt-BR/models/I18nCompanyBR

---
+++ src/userOnly/I18n/pt-BR/UserModel.js
@@ -0,0 +1,22 @@
+/**
+ * @module I18n/pt-BR/models/I18nCompanyBR
+ */
+
+export const I18nUserModelBR = {
+  id: 'id',
+  createdAt: 'Criado em',
+  updatedAt: 'Atualizado em',
+  email: 'E-mail',
+  userName: 'Nome',
+  active: 'Ativo',
+  code: 'Código',
+  password: 'Senha',
+  resetPasswordToken: 'Token para redefinir senha',
+  resetPasswordSentAt: 'Redefinição de senha enviada em',
+  rememberCreateAt: 'Lembre-se de criar em',
+  lastPasswordUpdate: 'Última atualização de senha',
+  passwordExpiration: 'Expiração de senha',
+  failedLoginAttempts: 'Tentativas de login falhadas',
+  lockoutTime: 'Tempo de bloqueio',
+  createdBy: 'Criado por'
+};
