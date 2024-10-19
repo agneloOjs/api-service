2b84a004d4a78fb6e53e581a144fd134911d09c1
agnelofh <tech.agnelo@gmail.com>
2024-10-19 16:55:08 -0300

    added 18n/pt-BR/message/ErrorMessagesBR

---
+++ src/shared/I18n/pt-BR/ErrorMessagesBR.js
@@ -0,0 +1,8 @@
+/**
+ * @module 18n/pt-BR/message/ErrorMessagesBR
+ */
+
+export const ERROR_MESSAGES_BR = {
+  INTERNAL_SERVER_ERROR: "Erro interno do servidor.",
+  ERROR_WHEN_STARTING_SERVER: "Erro ao iniciar o servidor",
+};
