aec62522b562232e55632246744867c514f21379
agnelofh <tech.agnelo@gmail.com>
2024-10-19 17:56:05 -0300

    added constants/logger

---
+++ src/shared/utils/Logger.js
@@ -0,0 +1,20 @@
+/**
+ * Constantes logger
+ * @module constants/logger
+ */
+
+import winston from "winston";
+
+const Logger = winston.createLogger({
+  level: "info",
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json(),
+  ),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: "logs/app.log" }),
+  ],
+});
+
+export default Logger;
