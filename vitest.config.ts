725ced5541bd10bb66af344357f9259b7685a18a
agnelofh <tech.agnelo@gmail.com>
2024-10-19 12:13:46 -0300

    refactoring files

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,12 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    include: ["**/__tests__/**/*.{test,spec}.{js,mjs,cjs}"],
+    globals: true,
+    environment: "jsdom",
+    coverage: {
+      reporter: ["text", "html"],
+    },
     browser: {
       enabled: true,
       headless: true,
