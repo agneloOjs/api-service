f46c35e2251f20c3771524e3524fb3dfcd79fbbf
agnelofh <tech.agnelo@gmail.com>
2024-10-19 16:11:29 -0300

    refactoring  vitestConfig

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,19 +1,12 @@
-import stencil from "unplugin-stencil/vite";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
     include: ["**/__tests__/**/*.{test,spec}.{js,mjs,cjs}"],
     globals: true,
-    environment: "jsdom",
+    environment: "node",
     coverage: {
       reporter: ["text", "html"],
     },
-    browser: {
-      enabled: true,
-      headless: true,
-      name: "chrome",
-    },
   },
-  plugins: [stencil()],
 });
