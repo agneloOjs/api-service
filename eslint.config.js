de796fda0a1351f5c41718eed6d9348356a76fce
agnelofh <tech.agnelo@gmail.com>
2024-10-19 16:09:19 -0300

    refactoring eslint.config

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,7 @@
-import js from "@eslint/js";
+import globals from "globals";
+import pluginJs from "@eslint/js";
 
 export default [
-  js.configs.recommended,
-
-  {
-    rules: {
-      "no-unused-vars": "warn",
-      "no-undef": "warn",
-    },
-  },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
 ];
