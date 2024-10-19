63048e0ed50a90ff0fc273346ed786bcde28a8fd
agnelofh <tech.agnelo@gmail.com>
2024-10-19 14:27:55 -0300

    added config/dbService.js

---
+++ src/config/dbService.js
@@ -0,0 +1,31 @@
+import { PrismaClient } from "@prisma/client";
+
+const dbService = new PrismaClient();
+
+/**
+ * Função para conectar ao banco de dados.
+ */
+export const conectDb = async () => {
+  try {
+    await dbService.$connect();
+    console.log("Conectado ao banco de dados.");
+  } catch (error) {
+    console.log("Erro ao conectar ao banco de dados.");
+    throw error;
+  }
+};
+
+/**
+ * Função para desconectar do banco de dados.
+ */
+export const desconectDb = async () => {
+  try {
+    await dbService.$disconnect();
+    console.log("Desconectado do banco de dados com sucesso.");
+  } catch (error) {
+    console.error("Error ao desconectar do banco de dados.");
+    throw error;
+  }
+};
+
+export default dbService;
