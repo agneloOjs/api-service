f78b8e140bd7b166f7e44d001bd7a163c743cfc1
agnelofh <tech.agnelo@gmail.com>
2024-10-19 12:04:27 -0300

    added model and migrate user

---
+++ prisma/migrations/20241019150125_user/migration.sql
@@ -0,0 +1,36 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" UUID NOT NULL,
+    "createdAt" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(6) NOT NULL,
+    "email" VARCHAR(80) NOT NULL,
+    "userName" VARCHAR(80) NOT NULL,
+    "active" BOOLEAN NOT NULL DEFAULT false,
+    "code" INTEGER NOT NULL,
+    "password" VARCHAR(255),
+    "resetPasswordToken" VARCHAR(255),
+    "resetPasswordSentAt" TIMESTAMP(3),
+    "rememberCreateAt" TIMESTAMP(3),
+    "lastPasswordUpdate" TIMESTAMP(6),
+    "passwordExpiration" TIMESTAMP(6),
+    "failedLoginAttempts" INTEGER DEFAULT 0,
+    "lockoutTime" TIMESTAMP(3),
+    "createdBy" UUID,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_id_key" ON "users"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_code_key" ON "users"("code");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_createdBy_key" ON "users"("createdBy");
+
+-- CreateIndex
+CREATE INDEX "users_email_code_idx" ON "users"("email", "code");
