619458428ff590add8250a041eb4d7d8dadd5bde
agnelofh <tech.agnelo@gmail.com>
2024-10-20 21:19:16 -0300

    added class UserCreateRepository

---
+++ src/userOnly/repositories/UserCreate.js
@@ -0,0 +1,17 @@
+import dbService from '../../config/dbService.js';
+
+export default class UserCreateRepository {
+  /**
+   * @param {Object} userData
+   * @returns {Promise<Object>}
+   * @throws {Error}
+   */
+
+  async create(userData) {
+    try {
+      return await dbService.user.create({ data: userData });
+    } catch (error) {
+      throw new Error(`Error creating user: ${error.message}`);
+    }
+  }
+}
