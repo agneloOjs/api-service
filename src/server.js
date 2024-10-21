a4c9989368f6d7d7dacb7c6cd97cc32f065d3e70
agnelofh <tech.agnelo@gmail.com>
2024-10-20 21:16:04 -0300

    refactoring server.js

--- src/server.js
+++ src/server.js
@@ -1,14 +1,12 @@
-// server.js
-
-import "dotenv/config";
-import app from "./app.js";
-import { connectDb, desconectDb } from "./config/dbService.js";
-import { ERROR_MESSAGES_BR } from "./shared/I18n/pt-BR/ErrorMessagesBR.js";
-import Logger from "./shared/utils/Logger.js";
+import 'dotenv/config';
+import app from './app.js';
+import { connectDb, desconectDb } from './config/dbService.js';
+import { ERROR_MESSAGES_BR } from './shared/I18n/pt-BR/ErrorMessagesBR.js';
+import Logger from './shared/utils/Logger.js';
 
 class Server {
   constructor() {
-    this.port = process.env.PORT;
+    this.port = process.env.API_PORT;
     this.url = process.env.CORS_ORIGIN;
   }
 
@@ -22,13 +20,13 @@ class Server {
 
       // Inicia o servidor após a conexão com o banco de dados.
       app.listen(this.port, () => {
-        console.log(`Servidor rodando na porta ${this.port}`);
-        console.log(`Ambiente: ${process.env.NODE_ENV || "development"}`);
         console.log(`Data/Hora: ${new Date().toISOString()}`);
+        console.log(`Ambiente: ${process.env.NODE_ENV || 'development'}`);
+        console.log(`Servidor rodando na porta ${this.url}:${this.port}`);
       });
 
       // Escutando sinal para desconectar antes de sair
-      process.on("SIGINT", async () => {
+      process.on('SIGINT', async () => {
         await desconectDb();
         process.exit(0);
       });
